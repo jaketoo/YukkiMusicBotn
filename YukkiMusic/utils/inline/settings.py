f2e34d010abadb11f8d35cb0f521a21812bdfe55
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2023-09-13 17:35:11 +0300

    Update settings.py

--- YukkiMusic/utils/inline/settings.py
+++ YukkiMusic/utils/inline/settings.py
@@ -27,11 +27,6 @@ def setting_markup(_):
                 text=_["ST_B_3"], callback_data="AU"
             ),
             InlineKeyboardButton(
-                text=_["ST_B_6"], callback_data="LG"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
                 text=_["ST_B_5"], callback_data="PM"
             ),
             InlineKeyboardButton(
